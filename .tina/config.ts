2ce7406b3f11aecc05712c1aeae26231c10aa010
Lucas Ernesto Kindinger <kindingerlek@hotmail.com>
2022-12-21 18:09:06 -0300

    tina cfg

--- .tina/config.ts
+++ .tina/config.ts
@@ -6,8 +6,8 @@
   
   export default defineConfig({
     branch,
-    clientId: null,   // Get this from tina.io
-    token: null,      // Get this from tina.io
+    clientId: "d3ee0d1d-aa57-4bc1-baa1-a0add291fe47",   // Get this from tina.io
+    token: "21566da53487d4083e9ec0ec03bdeea669e0a124",      // Get this from tina.io
     build: {
       outputFolder: "admin",
       publicFolder: "static",
@@ -24,6 +24,26 @@
           name: "post",
           label: "Posts",
           path: "content/posts",
+          fields: [
+            {
+              type: "string",
+              name: "title",
+              label: "Title",
+              isTitle: true,
+              required: true,
+            },
+            {
+              type: "rich-text",
+              name: "body",
+              label: "Body",
+              isBody: true,
+            },
+          ],
+        },
+        {
+          name: "docs",
+          label: "Docs",
+          path: "docs",
           fields: [
             {
               type: "string",
